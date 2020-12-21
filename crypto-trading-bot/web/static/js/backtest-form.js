5765bcbc2ae2abd26b35a1a422a422672580b301
AndyKeh <andy.keh@gmail.com>
2020-12-21 09:56:22 -0600

    second trading bot

---
+++ crypto-trading-bot/web/static/js/backtest-form.js
@@ -0,0 +1,34 @@
+$(function() {
+  $('form.backtest-form #form-strategies').change(function() {
+    // get data as string
+    const options = $(this)
+      .find('option:selected')
+      .attr('data-options');
+
+    if (options) {
+      $(this)
+        .closest('form')
+        .find('#form-options')
+        .val(options);
+    }
+  });
+  $('form.backtest-form #form-pair').change(function() {
+    // get data as string
+    const options = $(this)
+      .find('option:selected')
+      .attr('data-options');
+
+    if (options) {
+        const optionTag =  $(this)
+          .closest('form')
+          .find('#form-candle-period');
+        
+        optionTag.html('');
+        $.each(JSON.parse(options), function(key, value) {
+          optionTag
+          .append($('<option>', { value : value })
+          .text(value));
+     });
+    }
+  });  
+});
